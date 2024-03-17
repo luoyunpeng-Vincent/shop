90440f78ac5b1fed08406352d8da8987b36e4070
luoyunpeng <863037867@qq.com>
2024-03-17 23:43:26 +0800

    ceshi

--- hm-service/src/main/java/com/hmall/interceptor/LoginInterceptor.java
+++ hm-service/src/main/java/com/hmall/interceptor/LoginInterceptor.java
@@ -15,7 +15,7 @@ public class LoginInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        // 1.获取请求头中的 token
+        // 1.获取请求头中的 token ceshi
         String token = request.getHeader("authorization");
         // 2.校验token
         Long userId = jwtTool.parseToken(token);
